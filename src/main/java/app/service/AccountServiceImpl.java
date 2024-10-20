d0f395388c1724a960f262dda04157d015258fe3
temurka <temurka0@yandex.ru>
2024-10-20 19:51:47 +0300

    1

--- src/main/java/app/service/AccountServiceImpl.java
+++ src/main/java/app/service/AccountServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
-    private AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
 
     @Autowired
     public AccountServiceImpl(AccountRepository accountRepository) {
